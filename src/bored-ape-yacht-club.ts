85aa54c73c3da5cb93cb66a9c5b3a811a88e2f0e
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-11-04 23:38:25 +0100

    interacting with ape from smart contract and saving it to datastore

---
+++ src/bored-ape-yacht-club.ts
@@ -0,0 +1,23 @@
+import {
+  Transfer as TransferEvent,
+  BoredApeYachtClub as BoredApeYachtClubContract,
+} from "../generated/BoredApeYachtClub/BoredApeYachtClub";
+
+import { Ape, User } from "../generated/schema";
+
+export function handleTransfer(event: TransferEvent): void {
+  let ape = Ape.load(event.params.tokenId.toString());
+
+  if (!ape) {
+    ape = new Ape(event.params.tokenId.toString());
+
+    ape.creator = event.params.to.toHexString();
+    ape.createdAtTimestamp = event.block.timestamp;
+
+    let apeContract = BoredApeYachtClubContract.bind(event.address);
+    ape.contentURI = apeContract.tokenURI(event.params.tokenId);
+  }
+
+  ape.owner = event.params.to.toHexString();
+  ape.save();
+}
